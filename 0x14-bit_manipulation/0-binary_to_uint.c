e72671f9e292d4f4df0b097b281f167fcd2c17d5
Mpho Matli <mphojames9@gmail.com>
2023-05-10 06:19:31 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
+/**
+ *
 unsigned int binary_to_uint(const char *b)
 {
-int i;
+int j;
 unsigned int dec_val = 0;
 if (!b)
 return (0);
-for (i = 0; b[i]; i++)
+for (j = 0; b[j]; j++)
 {
-if (b[i] < '0' || b[i] > '1')
+if (b[j] < '0' || b[j] > '1')
 return (0);
-dec_val = 2 * dec_val + (b[i] - '0');
+dec_val = 2 * dec_val + (b[j] - '0');
 }
 return (dec_val);
 }
