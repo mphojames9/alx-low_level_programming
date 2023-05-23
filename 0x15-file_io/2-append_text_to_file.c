fe7fac525c15193c823a435c7b571e2fd13a75da
Mpho Matli <mphojames9@gmail.com>
2023-05-23 11:58:00 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,7 +10,7 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int i, q, len = 0;
+int o, w, len = 0;
 if (filename == NULL)
 return (-1);
 if (text_content != NULL)
@@ -18,11 +18,11 @@ if (text_content != NULL)
 for (len = 0; text_content[len];)
 len++;
 }
-i = open(filename, O_WRONLY | O_APPEND);
-q = write(i, text_content, len);
+o = open(filename, O_WRONLY | O_APPEND);
+w = write(o, text_content, len);
 if (i == -1 || w == -1)
 return (-1);
-close(i);
+close(o);
 return (1);
 }
 
